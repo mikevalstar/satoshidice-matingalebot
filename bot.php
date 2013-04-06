35bbe9cb1f036b432b71f9075c66bc7360903516
Mike Valstar <mikevalstar@gmail.com>
2013-04-06 10:45:05 -0400

    - added timezone - Validate address - Set the transaction fee

--- bot.php
+++ bot.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('America/Toronto'); // See: http://www.php.net/manual/en/timezones.php to change timezone
 require_once('./lib/jsonRPCClient.php');
 require_once('./lib/Logging.php');
 require_once('./lib/functions.php');
@@ -6,13 +7,18 @@ require_once('./lib/functions.php');
 
 define('MIN_BET', 0.01);                                // Minimum Bet
 define('MAX_BET', 0.10);                                // Maximum Bet
-define('ADDRESS', '1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp'); // Satoshi-Dice Adress to Bet on
+define('TX_FEE', 0.0005);                                // Transaction fee to use
+define('ADDRESS', '1dice9wcMu5hLF4g81u8nioL5mmSHTApw'); // Satoshi-Dice Adress to Bet on
 define('MULTIPLIER', 2.0);				// multiplier to up bets at
-define('MAX_GAMES', 10);                               // Stop after 250 Won Games
+define('MAX_GAMES', 100);                               // Stop after X Won Games
 define('RPC_USER', 'mikevalstar');                           //
 define('RPC_PASS', 'password');
 
 $b = new jsonRPCClient('http://'.RPC_USER.':'.RPC_PASS.'@127.0.0.1:8332/');
+
+// Validate the bitcoin address
+$validate = $b->validateaddress(ADDRESS);
+if($validate['isvalid'] != 1) die('Bitcoin address is not valid, stopping bot.');
 
 $bet = MIN_BET;
 $total_fees = 0;
@@ -48,6 +54,7 @@ while (($bet <= MAX_BET) && ($count_won < MAX_GAMES))
     }
 	try // Wrapped in a try catch block just incase we run out of cash.
     {
+        $b->settxfee(TX_FEE);
         $transaction_id = $b->sendtoaddress(ADDRESS, (float) $bet);
 	}
     catch(Exception $e)
